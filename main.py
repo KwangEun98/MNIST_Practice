d4e3f1e66a13935de5f074c807c43c649ba5b1ac
KwangEun98 <113772703+KwangEun98@users.noreply.github.com>
2024-04-07 22:03:52 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import argparse
 
-from models import resnet, simpleMLP
+from models import resnet, simpleMLP, VGG
 from data.data_loader import MNIST_loader
+from torch.utils.tensorboard import Summarywriter
 from train import *
 
 import train
 
+## changeable part
+
+parser = argparse.ArgumentParser(description = 'train part of MNIST')
+parser.add_argument('--lr', default = 0.1, type = float)
+parser.add_argument('--epochs', default = 100, type = int)
+parser.add_argument('--dropout', default = 0.1, type = float)
+parser.add_argument('--model', choices= ['ResNet', 'SimpleMLP', 'VGG'], type = str)
+args = parser.parse_args()
+
+## Fixed Part
 
 CONFIG = {
-    'lr': 0.1,
-    'epochs': 100,
-    'min_batch': 128,
     'dropout': 0.20,
     'weight_decay': 1e-4
 }
 
-model = resnet.ResNet18([2,2,2,2], input_channel = 1).to(device = 'cuda:0' if torch.cuda.is_available() else 'cpu')
+
+if args.model == 'ResNet':
+    model = resnet.ResNet18([2,2,2,2], input_channel = 1).to(device = 'cuda:0' if torch.cuda.is_available() else 'cpu')
+
+if args.model == 'VGG':
+    model = VGG.VGG_19(init_channel = 1, blocks = [2,2,2,2,4], dropout_p=args.dropout).to(device = 'cuda:0' if torch.cuda.is_available() else 'cpu')
+
 loss_fn = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=CONFIG['lr'], weight_decay=CONFIG['weight_decay'])
-# train_step = make_train_step(model, loss_fn, optimizer)
+optimizer = optim.SGD(model.parameters(), lr=args.lr, weight_decay=CONFIG['weight_decay'])
 
 train_loader, test_loader = MNIST_loader(batch_size = 32)
 
-train_model(model, train_loader, test_loader, loss_fn, optimizer)
+train_model(model, train_loader, test_loader, loss_fn, optimizer, epochs = args.epochs, eval_test_accuracy = True)
